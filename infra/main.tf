3e8d2768132b0d94214257c92e528cae6e74fad8
Abhiram-Rakesh <abhiramrakesh@gmail.com>
2026-02-23 10:01:50 +0530

    chore: split providers.tf from main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version = ">= 1.7.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-  backend "s3" {}
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "random_id" "bucket_suffix" {
   byte_length = 8
 }
