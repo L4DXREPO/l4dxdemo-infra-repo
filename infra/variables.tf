80eb63d08ad7566939508044f65dec8e0bf4de70
Abhiram-Rakesh <abhiramrakesh@gmail.com>
2026-02-24 20:12:23 +0530

    fix: add default values to variables for CI

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,8 @@
 
-
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "ap-south-1"
 }
 
 #PRODUCTION VPC
@@ -10,21 +10,25 @@ variable "region" {
 variable "production_vpc_cidr" {
   description = "CIDR block for production VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "production_subnet1_cidr" {
   description = "CIDR block for production private subnet 1"
   type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "production_subnet2_cidr" {
   description = "CIDR block for production private subnet 2"
   type        = string
+  default     = "10.0.2.0/24"
 }
 
 variable "production_subnet3_cidr" {
   description = "CIDR block for production private subnet 3"
   type        = string
+  default     = "10.0.3.0/24"
 }
 
 
@@ -32,14 +36,16 @@ variable "production_subnet3_cidr" {
 #NETWORK VPC
 
 variable "network_vpc_cidr" {
-  type = string
+  type    = string
+  default = "10.100.0.0/16"
 }
 
 variable "network_public_subnet_cidr" {
-  type = string
+  type    = string
+  default = "10.100.1.0/24"
 }
 
 variable "network_private_subnet_cidr" {
-  type = string
+  type    = string
+  default = "10.100.2.0/24"
 }
-
