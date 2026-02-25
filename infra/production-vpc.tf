37685e8b8509295fefdf41ea821f4ac997c512c0
razeeka <razeekamumtaz5828@gmail.com>
2026-02-25 14:43:43 +0530

    Updated production vpc with 1 pub subnet,2 private subnets, IGW, NAT and deleted network-vpc.tf and tgw.tf

--- infra/production-vpc.tf
+++ infra/production-vpc.tf
@@ -11,22 +11,22 @@ resource "aws_vpc" "production_vpc" {
 }
 
 
-# Private Subnet 1
+# Public Subnet 1
 
 
-resource "aws_subnet" "private_subnet_1" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.production_vpc.id
   cidr_block        = var.production_subnet1_cidr
   
 
   tags = {
-    Name = "production-private-subnet-1"
+    Name = "production-public-subnet-1"
   }
 }
 
 # Private Subnet 2
 
-resource "aws_subnet" "private_subnet_2" {
+resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.production_vpc.id
   cidr_block        = var.production_subnet2_cidr
 
@@ -38,7 +38,7 @@ resource "aws_subnet" "private_subnet_2" {
 # Private Subnet 3
 
 
-resource "aws_subnet" "private_subnet_3" {
+resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.production_vpc.id
   cidr_block        = var.production_subnet3_cidr
  
@@ -47,10 +47,64 @@ resource "aws_subnet" "private_subnet_3" {
     Name = "production-private-subnet-3"
   }
 }
+
+# INTERNET GATEWAY
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.production_vpc.id
+
+  tags = {
+    Name = "production-igw"
+  }
+}
+
+# ELASTIC IP FOR NAT
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+
+# NAT GATEWAY (Placed in Public Subnet)
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "production-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+# Public Route Table
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.production_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "production-public-rt"
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 # Private Route Table
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.production_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
 
   tags = {
     Name = "production-private-rt"
@@ -58,6 +112,8 @@ resource "aws_route_table" "private_route_table" {
 }
 
 # Route Table Associations
+
+
 
 resource "aws_route_table_association" "private_assoc_1" {
   subnet_id      = aws_subnet.private_subnet_1.id
@@ -69,22 +125,5 @@ resource "aws_route_table_association" "private_assoc_2" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
-resource "aws_route_table_association" "private_assoc_3" {
-  subnet_id      = aws_subnet.private_subnet_3.id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-# PRODUCTION ROUTE TO NETWORK VIA TGW
 
 
-resource "aws_route" "production_to_network" {
-  route_table_id         = aws_route_table.private_route_table.id
-  destination_cidr_block = var.network_vpc_cidr
-  transit_gateway_id     = aws_ec2_transit_gateway.main_tgw.id
-}
-# PRODUCTION TO INTERNET VIA NETWORK
-resource "aws_route" "production_to_internet_via_network" {
-  route_table_id         = aws_route_table.private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = aws_ec2_transit_gateway.main_tgw.id
-}
