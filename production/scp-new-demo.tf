2bbf2980b6ee28b05fb1834c696898f64a2a8f89
Aiswaryaraghusp <aiswaryaraghu2000@gmail.com>
2026-03-09 16:38:17 +0530

    Add new S3 bucket

--- production/scp-new-demo.tf
+++ production/scp-new-demo.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "scp_test_bucket" {
   bucket = "scp-test-bucket-aiswarya-002"
 
   tags = {
-    Name        = "scp-test-bucket134"
+    Name        = "scp-test-bucket90023"
     Environment = "test"
   }
 }
