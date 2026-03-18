929b25ff38458f37501a60835d34c7cd8c5ae641
Abhiram-Rakesh <abhiramrakesh@gmail.com>
2026-03-18 17:28:18 +0530

    Removed all commented out code 3

--- production/sg_production.tf
+++ production/sg_production.tf
@@ -1,27 +1,25 @@
-# Production SG — commented out until DC and standalone app are configured on on-prem side
-#
-# resource "aws_security_group" "production" {
-#   name        = "production-sg"
-#   description = "Production SG - all traffic inbound and outbound"
-#   vpc_id      = aws_vpc.production.id
-#
-#   ingress {
-#     description = "All inbound"
-#     protocol    = "-1"
-#     from_port   = 0
-#     to_port     = 0
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   egress {
-#     description = "All outbound"
-#     protocol    = "-1"
-#     from_port   = 0
-#     to_port     = 0
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   tags = merge(local.common_tags, {
-#     Name = "production-sg"
-#   })
-# }
+resource "aws_security_group" "production" {
+  name        = "production-sg"
+  description = "Production SG - all traffic inbound and outbound"
+  vpc_id      = aws_vpc.production.id
+
+  ingress {
+    description = "All inbound"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "production-sg"
+  })
+}
