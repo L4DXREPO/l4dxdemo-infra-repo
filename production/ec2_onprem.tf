2584ecf38c41286841de02a25d2dd89aff3be038
Abhiram-Rakesh <abhiramrakesh@gmail.com>
2026-03-24 12:00:10 +0530

    Fix onprem-standalone-server boot failure, rebuild initramfs, install MGN agent, and update AMI to ami-0504a251fce17b8c8

--- production/ec2_onprem.tf
+++ production/ec2_onprem.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "onprem_standalone" {
-  ami                         = "ami-0edfff5f156b4ea95"
+  ami                         = "ami-0504a251fce17b8c8"
   instance_type               = "t3.medium"
   subnet_id                   = aws_subnet.onprem_public.id
   vpc_security_group_ids      = [aws_security_group.onprem.id]
